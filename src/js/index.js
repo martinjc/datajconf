64419c83046a8debb73ec5b05b2d1fed4f674a76
Martin <martin.chorley@gmail.com>
2019-01-03 11:46:19 +0000

    first 2019 update

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,21 @@
 import * as moment from 'moment';
 
-const CONFERENCE_DATE = new moment('2017-06-19 09:00');
+const CONFERENCE_DATE = new moment('2019-07-01 09:00');
 
 function updateTimer() {
     var now = moment();
-    var diff = moment.duration(now.diff(CONFERENCE_DATE));
+    var diff = moment.duration(CONFERENCE_DATE.diff(now));
     var month_span = document.querySelector('span.months');
-    month_span.innerHTML = 11 - diff.months();
+    month_span.innerHTML = diff.months();
     var days_span = document.querySelector('span.days');
-    days_span.innerHTML = CONFERENCE_DATE.daysInMonth() - diff.days();
+    days_span.innerHTML = diff.days();
     var hours_span = document.querySelector('span.hours');
-    hours_span.innerHTML = 24 - diff.hours();
+    hours_span.innerHTML = diff.hours();
     var minutes_span = document.querySelector('span.minutes');
-    minutes_span.innerHTML = 60 - diff.minutes();
+    minutes_span.innerHTML = diff.minutes();
 }
 
-document.onload = function() {
+document.onload = function () {
     updateTimer();
     var timer = setInterval(updateTimer, 60000);
 }();
